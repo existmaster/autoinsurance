5cb5aecc7a01a737e0f6ac538977ffcf2638306c
existmaster <existmaster@gmail.com>
2018-05-22 21:01:01 +0900

    첫단계 중간저장

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,13 +2,14 @@
     <div>
         <div class="title">Auto Insurance</div>
         <div class="demo">Demo</div>
-        <b-button class="btn-start" size="sm" variant="primary" @click="nextPath">
+        <b-button class="btn-start" size="sm" variant="info" @click="nextPath">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15px" height="25px" viewBox="0 0 50 80" xml:space="preserve">
                 <polyline fill="none" stroke="#FFFFFF" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8"/>
             </svg>
         </b-button>
     </div>
 </template>
+
 <script>
 export default {
     name: 'index',
--- src/components/RegisterCustomer.vue
+++ src/components/RegisterCustomer.vue
@@ -1,15 +1,85 @@
 <template>
     <div class="content-area">
         <div class="title">Register Customer</div>
+        <div class="content">
+            <div class="input-area">
+                <b-input-group prepend="First Name">
+                    <b-form-input />
+                </b-input-group>
+                <b-input-group prepend="Last Name">
+                    <b-form-input />
+                </b-input-group>
+                <b-input-group prepend="Birth Date">
+                    <datepicker :bootstrapStyling="true" />
+                    <b-input-group-append>
+                        <b-btn variant="info">
+                            <icon name="calendar-alt"></icon>
+                        </b-btn>
+                    </b-input-group-append>
+                </b-input-group>
+                <b-input-group prepend="Street Address">
+                    <b-form-input />
+                </b-input-group>
+                <b-input-group prepend="Apt Unit Number">
+                    <b-form-input />
+                </b-input-group>
+                <b-input-group prepend="City">
+                    <b-form-input />
+                </b-input-group>
+                <b-input-group prepend="State">
+                    <b-form-input />
+                </b-input-group>
+                <b-input-group prepend="Zipcode">
+                    <b-form-input />
+                </b-input-group>
+            </div>
+        </div>
     </div>
 </template>
 
+<script>
+import Datepicker from 'vuejs-datepicker';
+
+export default {
+    name: 'register-customer',
+    components: {
+        Datepicker
+    },
+    data() {
+        return {
+            birthDate: new Date(),
+        }
+    },
+    methods: {
+    },
+}
+</script>
+
 <style scoped>
-    .content-area {
-        width: 100% !important;
+    .title {
+        font-size: 30px;
+        font-weight: bold;
     }
     .content {
+        width: 100%;
         margin: 20px;
-        /* border: 1px #333333 solid; */
+    }
+    .input-area {
+        position: relative;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 800px;
+        height: inherit;
+    }
+    .input-group {
+        margin: 10px 0 10px 0;
+        width: 80%;
+    }
+    .input-group-text {
+        font-size: 13px;
+    }
+    .vdp-datepicker {
+        width: 80%;
     }
 </style>
